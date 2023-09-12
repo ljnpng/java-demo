c17550b281f5fbca5b1e9872dc354805ae3467fa
neo <ljpfuture@gmail.com>
2023-09-12 20:43:42 +0800

    延迟队列 重试

--- java-spring/java-spring-rabbitmq/src/main/java/me/lyou/rabbit/handler/MyMessageHandler.java
+++ java-spring/java-spring-rabbitmq/src/main/java/me/lyou/rabbit/handler/MyMessageHandler.java
@@ -1,5 +1,6 @@
 package me.lyou.rabbit.handler;
 
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
@@ -9,28 +10,34 @@ public class MyMessageHandler {
 
     private final RabbitTemplate rabbitTemplate;
 
+    private static final int MAX_RETRIES = 4;
+
     public MyMessageHandler(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
     }
 
     @RabbitListener(queues = "main_queue")
-    public void handle(String message) {
-        System.out.println("consume message: " + message);
-        if (message.equals("bad")) {
-            throw new RuntimeException("bad message");
+    public void handle(Message message) {
+        String msg = new String(message.getBody());
+        int retry = message.getMessageProperties().getHeader("retry");
+        try {
+            System.out.println("consume message: " + msg);
+            if (msg.equals("bad")) {
+                throw new RuntimeException("bad message");
+            }
+        } catch (Exception e) {
+            if (retry < MAX_RETRIES) {
+                System.out.println("add to retry queue retry = " + retry);
+                rabbitTemplate.convertAndSend("delay_exchange", "delay", message, m -> {
+                    m.getMessageProperties().setHeader("retry", retry + 1);
+                    m.getMessageProperties().setExpiration(String.valueOf(10000 * (retry + 1)));
+                    return m;
+                });
+            } else {
+                System.out.println("Oh no! Game over!");
+            }
         }
-    }
-
-    @RabbitListener(queues = "dead_letter_queue")
-    public void handleDelay(String message) {
-        System.out.println("get dead letter message: " + message);
-
-        message = "dead letter message: " + message;
-
-        // do something
-        // report message to another queue
-        rabbitTemplate.convertAndSend("main", "main_queue", message);
-
 
     }
+
 }
--- java-spring/java-spring-rabbitmq/src/main/java/me/lyou/rabbit/sender/MyMessageSender.java
+++ java-spring/java-spring-rabbitmq/src/main/java/me/lyou/rabbit/sender/MyMessageSender.java
@@ -1,7 +1,11 @@
 package me.lyou.rabbit.sender;
 
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/rabbit")
@@ -16,6 +20,10 @@ public class MyMessageSender {
     @GetMapping("/send/{message}")
     public void send(@PathVariable String message) {
         System.out.println("send message: " + message);
-        this.rabbitTemplate.convertAndSend("main_exchange", "main_queue", message);
+        this.rabbitTemplate.convertAndSend("main_exchange", "main_queue", message, msg -> {
+            MessageProperties messageProperties = msg.getMessageProperties();
+            messageProperties.setHeader("retry", 0);
+            return msg;
+        });
     }
 }
